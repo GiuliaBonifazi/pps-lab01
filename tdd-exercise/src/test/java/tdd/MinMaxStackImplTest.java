36f090527f5d573308abdeb18071b95b4330342b
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2026-02-27 09:24:01 +0100

    Implemented peek and isEmpty on creation

--- tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
@@ -1,12 +1,26 @@
 package tdd;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
+    private MinMaxStack minMaxStack;
+
+    @BeforeEach
+    void beforeEach() {
+        minMaxStack = new MinMaxStackImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    void testIsEmptyOnCreation() {
+        assertTrue(minMaxStack.isEmpty());
+    }
+
+    @Test
+    void testPeekOnEmptyStack() {
+        assertThrows(IllegalStateException.class, () -> minMaxStack.peek());
     }
 }
